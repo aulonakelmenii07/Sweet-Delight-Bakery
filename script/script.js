c2f412f0250e2add9f3bb9581379cd29cc44970b
aulonakelmendi97@gmail.com <aulona.kelmendi@umib.net>
2026-01-14 17:24:45 +0100

    kam rregulluar footer

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
-function addToCart(pastry) {
-    alert(pastry + " added to cart 🛒");
-}
+//let buton = document.getElementById("button").addEventListener("click", addToCart);
+//function addToCart(pastry) {
+//    alert(pastry+" added to cart 🛒");
+//}
 
 function toggleMenu() {
     document.getElementById("navLinks").classList.toggle("show");
